90b80c51280971d0b0412dda8d58d213e241e791
Gabe Jackson <gabejackson@cxg.ch>
2014-07-15 14:52:46 +0200

    adaptation

--- djangocms_grid/forms.py
+++ djangocms_grid/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from djangocms_grid.models import Grid, GRID_CONFIG, DJANGOCMS_GRID_CHOICES
+from djangocms_grid.models import Grid, GRID_CONFIG, GRID_CHOICES
 from django.utils.translation import ugettext_lazy as _
 
 NUM_COLUMNS = [
@@ -9,7 +9,7 @@ NUM_COLUMNS = [
 
 class GridPluginForm(forms.ModelForm):
     create = forms.ChoiceField(choices=NUM_COLUMNS, label=_('Create Columns'), help_text=_('Create this number of columns inside'))
-    create_size = forms.ChoiceField(choices=DJANGOCMS_GRID_CHOICES, label=_('Column size'), help_text=('Width of created columns. You can still change the width of the column afterwards.'))
+    create_size = forms.ChoiceField(choices=GRID_CHOICES, label=_('Column size'), help_text=('Width of created columns. You can still change the width of the column afterwards.'))
 
     class Meta:
         model = Grid
